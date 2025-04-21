3f0f6de71b801a37b52631a8f16e4439a71849ad
RehanTaneja <81135520+RehanTaneja@users.noreply.github.com>
2025-04-21 18:52:00 -0400

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('copy', async () => {
-  try {
-    const text = await navigator.clipboard.readText();
-    chrome.runtime.sendMessage({ action: 'copiedText', text });
-  } catch (e) {
-    console.warn("Clipboard read failed:", e);
-  }
-});
