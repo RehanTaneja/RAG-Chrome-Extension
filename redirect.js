19ca3126e816f6d3a049091fd4c633c5d236cbf5
RehanTaneja <81135520+RehanTaneja@users.noreply.github.com>
2025-04-21 18:01:32 -0400

    Delete redirect.js

--- redirect.js
+++
@@ -1,4 +0,0 @@
-// redirect.js
-// Intercept every .pdf URL and load it in our viewer
-const pdfUrl = encodeURIComponent(location.href);
-location.replace(chrome.runtime.getURL(`viewer.html?file=${pdfUrl}`));
